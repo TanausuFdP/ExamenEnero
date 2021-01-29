4593079097b1d5f766e7263c866ffe8f4ed91c33
TanausuFdP <tana.vecin@hotmail.com>
2021-01-29 10:36:51 +0000

    *ADD: added RLE class. *ADD: added Tuple class.

---
+++ src/main/java/rle/RunLenghtEncode.java
@@ -0,0 +1,16 @@
+package rle;
+
+import java.util.ArrayList;
+import java.util.stream.Stream;
+
+public class RunLenghtEncode {
+    private ArrayList<Tuple> list;
+
+    public ArrayList<Tuple> encode(String decodedSentence){
+        return null;
+    }
+
+    public String decode(Stream<Tuple> tuplesStream){
+        return "";
+    }
+}
---
+++ src/main/java/rle/Tuple.java
@@ -0,0 +1,14 @@
+package rle;
+
+import lombok.Getter;
+
+@Getter
+public class Tuple {
+    private int count;
+    private char character;
+
+    public Tuple(char character){
+        this.count = 1;
+        this.character = character;
+    }
+}
